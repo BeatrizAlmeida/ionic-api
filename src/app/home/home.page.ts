ffe9d50283646952a8c1b5d80dbd669207f2b117
BeatrizAlmeida <biaramos810@gmail.com>
2021-02-17 16:49:34 -0300

    # Estilização mais de acordo com os princípios de UI e UX

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -21,7 +21,6 @@ export class HomePage implements OnInit {
     this.teamService.getTeamsApi().subscribe(
       (response) => {
         this.teams = response;
-        console.log(response);
       }
     );
   }
